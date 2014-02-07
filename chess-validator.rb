7f42590f5b8f96244427f109b4e9805e87a31c7f
Yungchih <yungchih_chen@yahoo.com>
2014-02-07 18:37:09 -0500

    Rename some class variables to local variables, and gave meaningful variable name

--- chess-validator.rb
+++ chess-validator.rb
@@ -1,39 +1,39 @@
 
-def load_position
+def load_board_position
 	sp_file = File.new("simple_board.txt", "r")
-	@board = sp_file.each_line.map(&:split)
+	board = sp_file.each_line.map(&:split)
 
 	sp_file.close 
 
 	@game ={}
 	(1..8).to_a.reverse.each_with_index do |rank,i|
 		("a".."h").each_with_index do |d,j|
-			# game["#{d}#{rank}"] = "#{i}#{d}#{rank}#{j}"
-			@game["#{d}#{rank}"] = "#{@board[i][j]}"
+			@game["#{d}#{rank}"] = "#{board[i][j]}"
 		end
 	end
 
 end
 
-def load_moves
-	sm_file = File.new("simple_moves.txt", "r")
+def load_moves_and_check_moves
 
-	@moves = sm_file.each_line.map(&:split)
+	sm_file = File.new("simple_moves.txt", "r")
+	moves = sm_file.each_line.map(&:split)
 
 	sm_file.close
 
-	@moves.each do |key, value|
-		check_move(key, value)
+	moves.each do |key, value|
+		check_each_move(key, value)
 	end
 end
 
-def check_move(move_from, move_to)
-	role = @game[move_from]
-	puts case role[1,1]
+def check_each_move(move_from, move_to)
+	piece_name = @game[move_from]
+	
+	puts case piece_name[1,1]
 		when "P"
-			pawn(role, move_from, move_to)
+			move_result_for_pawn(piece_name, move_from, move_to)
 		when "N" 
-			knight(role, move_from, move_to)
+			move_result_for_knight(piece_name, move_from, move_to)
 		when "Q" 
 			"Call Queen's move"
 		when "K" 
@@ -42,59 +42,58 @@ def check_move(move_from, move_to)
 			"Call Bishop's move"
 		when "R" 
 			"Call Rook's move"		
-		when "--"
-			"#{role} ILLEGAL"
+		when "-"
+			"#{piece_name} empty square ILLEGAL"
 		else
-			"#{role} ILLEGAL"
+			"#{piece_name} ILLEGAL"
 	end
 
 end
 
-def knight(role, move_from, move_to)
-	f_steps = move_from[1,1].to_i
-	t_steps = move_to[1,1].to_i
-	org_col = move_from[0,1].ord.to_i
-	to_col  = move_to[0,1]
-	allow_col = [(org_col+1).chr, (org_col-1).chr]
+def move_result_for_knight(piece_name, move_from, move_to)
+	from_position = move_from[1,1].to_i
+	to_position = move_to[1,1].to_i
+	original_column = move_from[0,1].ord.to_i
+	move_to_column  = move_to[0,1]
+	allow_col = [(original_column+1).chr, (original_column-1).chr]
 	
-	case role[0,1]
+	case piece_name[0,1]
 		when "w"
-			move_steps = t_steps - f_steps
+			move_steps = to_position - from_position
 		when "b"
-			move_steps = f_steps - t_steps
+			move_steps = from_position - to_position
 		else
-			 "Empty space!"
+			 "Unknow color!"
 	end	
 
-	if move_steps == 2 && allow_col.include?(to_col)
-	 	"#{role} wants to move from #{move_from} to #{move_to} - LEGAL"
+	if move_steps == 2 && allow_col.include?(move_to_column)
+	 	"#{piece_name} wants to move from #{move_from} to #{move_to} - LEGAL"
 	else
-		"#{role} wants to move from #{move_from} to #{move_to} - ILLEGAL"
+		"#{piece_name} wants to move from #{move_from} to #{move_to} - ILLEGAL"
 	end
 end
 
-def pawn(role, move_from, move_to)
+def move_result_for_pawn(piece_name, move_from, move_to)
 	move_steps = move_to[1,1].to_i - move_from[1,1].to_i
 
-	case role[0,1]
+	case piece_name[0,1]
 		when "w"
 			 move_steps = move_to[1,1].to_i - move_from[1,1].to_i
-
 		when "b"
 			 move_steps = move_from[1,1].to_i - move_to[1,1].to_i
 		else
-			 "Empty space!"
+			 "Unknow color!"
 	end	
 
 	move_col = move_to[0,1] == move_from[0.1] ? true : false 
 	if move_steps <= 2 && move_col
-		"#{role} wants to move from #{move_from} to #{move_to} - LEGAL"
+		"#{piece_name} wants to move from #{move_from} to #{move_to} - LEGAL"
 	else
-		"#{role} wants to move from #{move_from} to #{move_to} - ILLEGAL"
+		"#{piece_name} wants to move from #{move_from} to #{move_to} - ILLEGAL"
 	end
 
 end
 
-load_position
-load_moves
+load_board_position
+load_moves_and_check_moves
 
